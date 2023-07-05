2ce6a3e2757771f74c53079fd746ac04f4953944
Mingcai SHEN <archsh@gmail.com>
2023-07-05 09:34:40 +0800

    Updated.

--- ftp.go
+++ ftp.go
@@ -1,3 +1,16 @@
 package c2
 
 // ftp client defines here
+
+type FTPClient struct {
+}
+
+func (ftp FTPClient) Get(filename string, output string) error {
+	return nil
+}
+
+var defaultFTPClient *FTPClient
+
+func init() {
+	defaultFTPClient = &FTPClient{}
+}
--- http.go
+++ http.go
@@ -1,3 +1,11 @@
 package c2
 
 // http client defines here
+type HTTPClient struct {
+}
+
+var defaultHTTPClient *HTTPClient
+
+func init() {
+	defaultHTTPClient = &HTTPClient{}
+}
